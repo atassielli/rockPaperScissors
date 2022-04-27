530c83e68d297712ba2a2496249ac5ee0949b960
atassielli <anthonytassielli@gmail.com>
2022-04-26 19:09:14 -0700

    Create javascript function for computer selection in game
    computer will randomly return either rock, paper, or scissors

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log(1)
+//Write code to return random computer choice in rock paper scissors//
+
+function computerPlay(comp) {
+    const options = Math.floor(Math.random() * comp.length);
+    const compPlay = comp[options];
+    return compPlay
+}
+
+const array = ['Rock', 'Paper', 'Scissors'];
+const computerChoice = computerPlay(array);
+console.log(computerChoice);
