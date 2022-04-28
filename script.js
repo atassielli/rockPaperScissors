5455c301e698e6b111e483b4078059584483ac88
atassielli <anthonytassielli@gmail.com>
2022-04-27 20:18:07 -0700

    Add code to make the user input non case sensitive
    add toLowerCase() to both the playerChoice and computerChoice variables

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function computerPlay(comp) {
 }
 
 let array = ['Rock', 'Paper', 'Scissors'];
-let computerChoice = computerPlay(array);
+let computerChoice = computerPlay(array).toLowerCase();
 console.log(computerChoice);
 
 //Make a prompt that asks users for their selection from rock, paper, and scissors//
@@ -18,30 +18,30 @@ function userChoice() {
     return prompt(makeSelection.toString());
 }
 
-let playerChoice = userChoice().toString();
+let playerChoice = userChoice().toString().toLowerCase();
 console.log(playerChoice);
 
 
 //Create function that compares computerChoice with playerChoice to determine a winner//
 
 let result;
-if ((computerChoice == 'Rock') && (playerChoice.toString() == 'Paper')) {
+if ((computerChoice == 'rock') && (playerChoice.toString() == 'paper')) {
     result = 'Player wins, congratulations';
-} else if((computerChoice == 'Rock') && (playerChoice.toString() == 'Scissors')) {
+} else if((computerChoice == 'rock') && (playerChoice.toString() == 'scissors')) {
      result = 'Conputer wins, better luck next time';
-} else if((computerChoice == 'Paper') && (playerChoice.toString() == 'Rock')) {
+} else if((computerChoice == 'paper') && (playerChoice.toString() == 'rock')) {
     result = 'Conputer wins, better luck next time';
-} else if((computerChoice == 'Paper') && (playerChoice.toString() == 'Scissors')) {
+} else if((computerChoice == 'paper') && (playerChoice.toString() == 'scissors')) {
     result = 'Player wins, congratulations';
-} else if((computerChoice == 'Scissors') && (playerChoice.toString() == 'Rock')) {
+} else if((computerChoice == 'scissors') && (playerChoice.toString() == 'rock')) {
     result = 'Player wins, congratulations';
- } else if((computerChoice == 'Scissors') && (playerChoice.toString() == 'Paper')) {
+ } else if((computerChoice == 'scissors') && (playerChoice.toString() == 'paper')) {
     result = 'Conputer wins, better luck next time';
-} else if((computerChoice == 'Rock') && (playerChoice.toString() == 'Rock')) {
+} else if((computerChoice == 'rock') && (playerChoice.toString() == 'rock')) {
     result = 'Tie, please try again';
-} else if((computerChoice == 'Paper') && (playerChoice.toString() == 'Paper')) {
+} else if((computerChoice == 'paper') && (playerChoice.toString() == 'paper')) {
      result = 'Tie, please try again';
-} else if((computerChoice == 'Scissors') && (playerChoice.toString() == 'Scissors')) {
+} else if((computerChoice == 'scissors') && (playerChoice.toString() == 'scissors')) {
     result = 'Tie, please try again';
 }
 
