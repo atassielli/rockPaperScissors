82a08396400ffe37732af98a95202d7ec553f64b
atassielli <anthonytassielli@gmail.com>
2022-04-27 19:26:30 -0700

    Creat function that prompts user to input their choice
    prompt window appears asking the player to input rock, paper, or scissors. function returns the result

--- script.js
+++ script.js
@@ -6,11 +6,44 @@ function computerPlay(comp) {
     return compPlay
 }
 
-const array = ['Rock', 'Paper', 'Scissors'];
-const computerChoice = computerPlay(array);
+let array = ['Rock', 'Paper', 'Scissors'];
+let computerChoice = computerPlay(array);
 console.log(computerChoice);
 
 //Make a prompt that asks users for their selection from rock, paper, and scissors//
 
-let playerChoice = prompt('Choose Rock, Paper, or Scissors');
+let makeSelection = "Choose Rock, Paper, or Scissors";
+
+function playerChoice() {
+    return prompt(makeSelection);
+}
+
+console.log(playerChoice());
+
+
+//Create function that compares computerChoice with playerChoice to determine a winner//
+
+let result;
+if ((computerChoice = 'Rock') && (playerChoice = 'Paper')) {
+    result = 'Player wins, congratulations';
+} else if((computerChoice = 'Rock') && (playerChoice = 'Scissors')) {
+     result = 'Conputer wins, better luck next time';
+} else if((computerChoice = 'Paper') && (playerChoice = 'Rock')) {
+    result = 'Conputer wins, better luck next time';
+} else if((computerChoice = 'Paper') && (playerChoice = 'Scissors')) {
+    result = 'Player wins, congratulations';
+} else if((computerChoice = 'Scissors') && (playerChoice = 'Rock')) {
+    result = 'Player wins, congratulations';
+ } else if((computerChoice = 'Scissors') && (playerChoice = 'Paper')) {
+    result = 'Conputer wins, better luck next time';
+} else if((computerChoice = 'Rock') && (playerChoice = 'Rock')) {
+    result = 'Tie, please try again';
+} else if((computerChoice = 'Paper') && (playerChoice = 'Paper')) {
+     result = 'Tie, please try again';
+} else if((computerChoice = 'Scissors') && (playerChoice = 'Scissors')) {
+    result = 'Tie, please try again';
+}
+
+
+
 
